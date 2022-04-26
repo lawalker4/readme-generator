9ab62621adf2783791e8d3f4f8469560549ac70f
LA WALKER <lauraashleywalker4@gmail.com>
2022-04-26 10:42:39 -0400

    update README

--- README.md
+++ README.md
@@ -1,27 +1,48 @@
-#README Generator
+### README Generator
 
 ---
 ## Contents
-1.
+1. About
+2. Installation
+3. License
+4. Authors
+
 
 ---
 ## About
+This application has been created to help coders create a great README. They have to answer a few questions and there README has been generated. 
 
 ---
-### User Store
+### User Story
 ```
-dfafakjflkdjl
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
 ```
 
 ---
 ### Criteria
 ```
-dfadfaf
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+WHEN I enter my project title
+THEN this is displayed as the title of the README
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
 ```
 ---
 # Screenshots:
 
-![screenshot 1](image/readme-generator-1.png)
+1. ![screenshot 1](image/readme-generator-1.png)
 2
 3
 
@@ -30,9 +51,14 @@ dfadfaf
 ### Installation
 Repo:
 
+
 ---
 
 ## License
 
+
 ----
 ## Authors
+
+Built and maintain by [Ashley Walker](https://github.com/lawalker4)
+---
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 const gemerateAbout = aboutText => {
@@ -29,6 +29,8 @@ function generateMarkdown(data) {
 
   ## Description
   ${data.desc}
+
+  ---
   
   ## Table of Contents (Optional)
   
@@ -42,6 +44,7 @@ function generateMarkdown(data) {
   ## Installation
 ${data.installation}  
   
+  ---
   ## Usage
   ${data.usage}  
 
@@ -53,17 +56,12 @@ ${data.installation}
       md
       ![alt text](assets/images/screenshot.png)
       
+  ---
   
   ## Credits
   ${data.credits}  
 
-  
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
-  
+  --- 
   ## License
   ${data.license}  
 
@@ -71,29 +69,14 @@ ${data.installation}
   The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
   
   ---
-  
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  
-  ## Badges
-  ${data.badges}  
-
-  
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-  
-  ## Features
-  ${data.features}  
-
-  
-  If your project has a lot of features, list them here.
-  
+      
   ## How to Contribute
   ${data.contribute}  
-
+  ---
   
   If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
   
+  ---
   ## Tests
   ${data.tests}  
   
